1ca71527d0d7f4c41958ad1abd01b0b9e3dab614
atanu <atanuj383@gmail.com>
2022-11-13 09:40:09 +0530

    add

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  images: { domains: ['cdn.pixabay.com','rukminim1.flixcart.com'] },
-  env: {
-    MONGO_URI: "mongodb+srv://nextapp:nextapp@cluster0.8fig6.mongodb.net/nextEcom?retryWrites=true&w=majority",
-    TOKEN_SECRECT:'abababababababababab@'
-}
-}
-
+  images: { domains: ['cdn.pixabay.com', 'rukminim1.flixcart.com'] },
+  env: {},
+};
