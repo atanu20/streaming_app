00ead36d06b4f4236ffad2dc3a229db877a18041
atanu <atanuj383@gmail.com>
2022-11-13 19:24:56 +0530

    ad

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
-module.exports = {
+// module.exports = {
+//   reactStrictMode: true,
+//   images: { domains: ['cdn.pixabay.com', 'rukminim1.flixcart.com'] },
+//   env: {},
+// };
+// "eslint": "7.32.0",
+// "eslint-config-next": "12.0.1"
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ['cdn.pixabay.com', 'rukminim1.flixcart.com'] },
-  env: {},
+};
+
+module.exports = {
+  images: {
+    domains: ['images.unsplash.com', 'openweathermap.org'],
+  },
 };
